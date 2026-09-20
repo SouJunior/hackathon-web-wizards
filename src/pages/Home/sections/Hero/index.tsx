53426fde2960fb8ca740f4f86cab5f2164ebf6c8
Vanilo Ferreira <programacaoti09.vanilo@assectra.com.br>
2026-09-20 12:17:47 -0300

    feat: adiciona seção hero da landing page

---
+++ src/pages/Home/sections/Hero/index.tsx
@@ -0,0 +1,50 @@
+import { ContrastParagraph } from '@/styles/global';
+
+import {
+  HeroTitle,
+  HeroContainer,
+  AreasContent,
+  HeroTextContent,
+  ButtonContainer,
+  HeroMascote,
+  DesignLeft,
+  DesignRight,
+} from './styles';
+import { Button } from '@/components/Button';
+
+import Mascote from '@/assets/heroMascote.svg';
+import Design from '@/assets/designHero.svg';
+
+export function Hero() {
+  return (
+    <HeroContainer>
+      <AreasContent>
+        <DesignLeft>
+          <img src={Design} alt="" />
+        </DesignLeft>
+
+        <DesignRight>
+          <img src={Design} alt="" />
+        </DesignRight>
+        <HeroTextContent>
+          <HeroTitle>Mais de 50% das vagas pedem experiência!</HeroTitle>
+          <ContrastParagraph>
+            A SouJunior capacita talentos com experiências reais, preparando
+            profissionais para entrar no mercado com confiança.
+          </ContrastParagraph>
+        </HeroTextContent>
+        <ButtonContainer>
+          <Button to="/" $contrast>
+            Quero Apoiar!
+          </Button>
+          <Button to="/" $hideMobile>
+            Conheça o projeto!
+          </Button>
+        </ButtonContainer>
+        <HeroMascote>
+          <img src={Mascote} alt="Mascote da SouJunior" />
+        </HeroMascote>
+      </AreasContent>
+    </HeroContainer>
+  );
+}
--- src/styles/colorPalette.ts
+++ src/styles/colorPalette.ts
@@ -1,8 +1,10 @@
 export const colorPalette = {
   primaryColor: '#3C7EF9',
   secondaryColor: '#EFF4FF',
-  titleColor: '#1E47AF',
+  thirdColor: '#1E47AF',
   paragraphColor: '#1A1A1A',
   borderColor: '#8C8C8C',
   answerColor: '#666666',
+  contrastColor: '#FFFFFF',
+  backgroundColor: '#A9C4FD;',
 };

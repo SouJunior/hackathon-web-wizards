3ff266069d887fd8f68e0270549cc4675413308d
Renaneliziario <renanqueiroz92@gmail.com>
2026-09-24 18:55:15 -0300

    feat: adiciona link do formulário de apoio na 2ª pergunta do FAQ

--- src/pages/Home/sections/FAQ/FAQData.ts
+++ src/pages/Home/sections/FAQ/FAQData.ts
@@ -2,6 +2,8 @@ export interface QuestionsAndAnswersItem {
   id: number;
   question: string;
   answer: string;
+  linkLabel?: string;
+  linkUrl?: string;
 }
 
 export const QUESTIONS_AND_ANSWERS: QuestionsAndAnswersItem[] = [
@@ -13,9 +15,11 @@ export const QUESTIONS_AND_ANSWERS: QuestionsAndAnswersItem[] = [
   },
   {
     id: 2,
-    question: 'De que outra forma eu posso apoiar a SouJunior?',
+    question: 'Quais formas de apoio existem dentro da SouJunior?',
     answer:
-      'Além da contribuição financeira, você pode apoiar divulgando a SouJunior, indicando talentos e empresas parceiras ou compartilhando oportunidades que fortaleçam nossa comunidade.',
+      'Além da contribuição financeira, você pode apoiar de outras formas: divulgando a SouJunior nas suas redes (YouTube, LinkedIn e outras), participando como palestrante em nossos eventos ou se candidatando para atuar como voluntário em nossos projetos.',
+    linkLabel: 'Clique aqui.',
+    linkUrl: 'https://www.soujunior.tech/apoiar',
   },
   {
     id: 3,
